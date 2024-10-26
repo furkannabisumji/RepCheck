c35bb3d96175e45fa616cc9eaeecc5357fe12bb2
Furkan Nabi Sumji <103491179+furkannabisumji@users.noreply.github.com>
2024-10-26 13:31:21 +0530

    improved the ui

--- frontend/nextjs/app/page.tsx
+++ frontend/nextjs/app/page.tsx
@@ -25,30 +25,36 @@ const Home = () => {
   const displayPoints = points ? points.toString() : '0';
 
   return (
-    <div className="min-h-screen bg-black flex text-white justify-center items-center">
+    <div className="min-h-screen bg-img flex text-white justify-center items-center">
+      <div className="absolute top-0 left-0 p-4">
+      <w3m-account-button />
+        </div>
       <div className="text-center">
         {!isConnected ? (
           <w3m-connect-button />
         ) : (
           <div>
-            <p className="mb-4"><w3m-account-button /></p>
             {isRegistered ? (
               <div className="h-16 w-48 border border-pink-600 rounded-3xl flex justify-between p-4">
                 <p className="text-gray-400 text-lg">Points</p>
                 <p className="text-green-700 text-lg">{displayPoints}</p>
               </div>
             ) : (
+              <div className="h-48 border border-pink-600 rounded-3xl flex flex-col gap-3 p-4 justify-center items-center">
+              <p className="text-gray-400 text-5xl">You did not register 🥺
+              </p>
               <button
                 onClick={() => writeContract({
                   abi: config.abi,
                   address: `0x${config.address}`,
                   functionName: 'register',
                 })}
-                className="bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded disabled:opacity-50"
+                className="bg-white hover:bg-pink-600 text-black font-bold py-2 px-2 w-20 rounded disabled:opacity-50"
                 disabled={isPending}
               >
                 Register
               </button>
+              </div>
             )}
           </div>
         )}
--- frontend/nextjs/tailwind.config.ts
+++ frontend/nextjs/tailwind.config.ts
@@ -8,10 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
+        backgroundImage: {
+          'img': "url('/bg.webp')",
+        },
     },
   },
   plugins: [],
